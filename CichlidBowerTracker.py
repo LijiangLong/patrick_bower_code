7f13bf1edbb3879a8af7782cc0311ed81d80ee70
Patrick McGrath <ptmcgrat@gmail.com>
2019-11-18 14:16:05 -0500

    Test

--- CichlidBowerTracker.py
+++ CichlidBowerTracker.py
@@ -88,7 +88,7 @@ if args.command == 'TotalProjectAnalysis':
 	for projectID in args.ProjectIDs:
 		downloadProcess = subprocess.run(['python3', 'CichlidBowerTracker.py', 'ProjectAnalysis', 'Download', projectID])
 		depthProcess = subprocess.Popen(['python3', 'CichlidBowerTracker.py', 'ProjectAnalysis', 'Depth', projectID, '-w', '1'])
-		clusterProcess = subprocess.Popen(['python3', 'CichlidBowerTracker.py', 'Cluster', 'Depth', projectID, '-w', '23'])
+		clusterProcess = subprocess.Popen(['python3', 'CichlidBowerTracker.py', 'ProjectAnalysis', 'Cluster', projectID, '-w', '23'])
 		depthProcess.communicate()
 		clusterProcess.communicate()
 		downloadProcess = subprocess.run(['python3', 'CichlidBowerTracker.py', 'ProjectAnalysis', 'Backup', projectID])
