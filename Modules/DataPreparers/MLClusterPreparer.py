2cc19f60a83bde231d4a2c29b59ced87813e3f8f
Patrick McGrath <patrick.mcgrath@biology.gatech.edu>
2019-11-23 06:35:00 -0500

    modify args

--- Modules/DataPreparers/MLClusterPreparer.py
+++ Modules/DataPreparers/MLClusterPreparer.py
@@ -44,21 +44,21 @@ class MLClusterPreparer:
 	def _prepareClips(self):
 		print('Converting clips into jpgs and calculating means,,Time: ' + str(datetime.datetime.now())) 
 
-		clips = [x for x in os.listdir(self.prFileManager.localAllClipsDir) if '.mp4' in x]
+		clips = [x for x in os.listdir(self.projFileManager.localAllClipsDir) if '.mp4' in x]
 		assert len(clips) != 0
 
-		with open(self.prFileManager.localMasterDir + 'MeansAll.csv', 'w') as f, open(self.prFileManager.localMasterDir + 'cichlids_test_list.txt', 'w') as g:
+		with open(self.projFileManager.localMasterDir + 'MeansAll.csv', 'w') as f, open(self.projFileManager.localMasterDir + 'cichlids_test_list.txt', 'w') as g:
 			print('Clip,MeanR,MeanG,MeanB,StdR,StdG,StdB', file = f)
 			for clip in clips:
 				label = self.videoClasses[0] # Need to temporarily assign the clip to a label - just pick the first
 					
-				outDirectory = self.prFileManager.localProcessedClipsDir + label + '/' + clip.replace('.mp4','') + '/'
+				outDirectory = self.projFileManager.localProcessedClipsDir + label + '/' + clip.replace('.mp4','') + '/'
 
 				shutil.rmtree(outDirectory) if os.path.exists(outDirectory) else None
 				os.makedirs(outDirectory) 
 
-				outdata = subprocess.run(['ffmpeg', '-i', self.prFileManager.localAllClipsDir + clip, outDirectory + 'image_%05d.jpg'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-				#print(['ffmpeg', '-i', self.prFileManager.localAllClipsDir + clip, outDirectory + 'image_%05d.jpg'])
+				outdata = subprocess.run(['ffmpeg', '-i', self.projFileManager.localAllClipsDir + clip, outDirectory + 'image_%05d.jpg'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+				#print(['ffmpeg', '-i', self.projFileManager.localAllClipsDir + clip, outDirectory + 'image_%05d.jpg'])
 
 				frames = [x for x in os.listdir(outDirectory) if '.jpg' in x]
 				try:
@@ -76,19 +76,19 @@ class MLClusterPreparer:
 				print(clip.replace('.mp4', '') + ',' + ','.join([str(x) for x in mean]) + ',' + ','.join([str(x) for x in std]), file = f)
 				print(label + '/' + clip.replace('.mp4',''), file = g)
 
-		subprocess.run(['touch', self.prFileManager.localMasterDir + 'cichlids_train_list.txt'])
+		subprocess.run(['touch', self.projFileManager.localMasterDir + 'cichlids_train_list.txt'])
 
 
-		dt = pd.read_csv(self.prFileManager.localMasterDir + 'MeansAll.csv', sep = ',')
+		dt = pd.read_csv(self.projFileManager.localMasterDir + 'MeansAll.csv', sep = ',')
 		dt['MeanID'] = dt.apply(lambda row: row.Clip.split('__')[0], axis = 1)
 		means = dt.groupby('MeanID').mean()
 
-		with open(self.prFileManager.localMasterDir + 'Means.csv', 'w') as f:
+		with open(self.projFileManager.localMasterDir + 'Means.csv', 'w') as f:
 			print('meanID,redMean,greenMean,blueMean,redStd,greenStd,blueStd', file = f)
 			for row in means.itertuples():
 				print(row.Index + ',' + str(row.MeanR) + ',' + str(row.MeanG) + ',' + str(row.MeanB) + ',' + str(row.StdR) + ',' + str(row.StdG) + ',' + str(row.StdB), file = f)
 
-		with open(self.prFileManager.localMasterDir + 'AnnotationFile.csv', 'w') as f:
+		with open(self.projFileManager.localMasterDir + 'AnnotationFile.csv', 'w') as f:
 			print('Location,Dataset,Label,MeanID', file = f)
 			for row in dt.itertuples():
 				print(row.Clip + ',Test,' + label + ',' + row.MeanID, file = f)
@@ -97,9 +97,9 @@ class MLClusterPreparer:
 		command = []
 		command += ['python3', self.mlFileManager.localVideoPythonJsonFile]
 		command += [self.mlFileManager.localVideoClassesFile]
-		command += [self.prFileManager.localMasterDir + 'cichlids_train_list.txt']
-		command += [self.prFileManager.localMasterDir + 'cichlids_test_list.txt']
-		command += [self.prFileManager.localMasterDir + 'cichlids.json']
+		command += [self.projFileManager.localMasterDir + 'cichlids_train_list.txt']
+		command += [self.projFileManager.localMasterDir + 'cichlids_test_list.txt']
+		command += [self.projFileManager.localMasterDir + 'cichlids.json']
 		subprocess.call(command)
 
 	def _predictLabels(self):
@@ -110,18 +110,18 @@ class MLClusterPreparer:
 		with open(self.mlFileManager.localVideoCommandsFile, 'rb') as pickle_file:
 			command = pickle.load(pickle_file) 
 		
-		command['--root_path'] = self.prFileManager.localMasterDir
+		command['--root_path'] = self.projFileManager.localMasterDir
 		command['--n_epochs'] = '1'
 		command['--pretrain_path'] = self.mlFileManager.localVideoModelFile
-		command['--mean_file'] = self.prFileManager.localMasterDir + 'Means.csv'
-		command['--annotation_file'] = self.prFileManager.localMasterDir + 'AnnotationFile.csv'
+		command['--mean_file'] = self.projFileManager.localMasterDir + 'Means.csv'
+		command['--annotation_file'] = self.projFileManager.localMasterDir + 'AnnotationFile.csv'
 		command['--annotation_path'] = 'cichlids.json'
 		command['--batch_size'] = str(int(int(command['--batch_size'])*2))
 		command['--video_path'] = 'AllClips'
 
 		resultsDirectory = 'prediction/'
-		shutil.rmtree(self.prFileManager.localMasterDir + resultsDirectory) if os.path.exists(self.prFileManager.localMasterDir + resultsDirectory) else None
-		os.makedirs(self.prFileManager.localMasterDir + resultsDirectory) 
+		shutil.rmtree(self.projFileManager.localMasterDir + resultsDirectory) if os.path.exists(self.projFileManager.localMasterDir + resultsDirectory) else None
+		os.makedirs(self.projFileManager.localMasterDir + resultsDirectory) 
 
 		trainEnv = os.environ.copy()
 		trainEnv['CUDA_VISIBLE_DEVICES'] = str(6)
@@ -135,13 +135,13 @@ class MLClusterPreparer:
 		outdata = subprocess.run(outCommand, env = trainEnv, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 		print(outdata.stderr)
 
-		dt = pd.read_csv(self.prFileManager.localMasterDir + '/prediction/ConfidenceMatrix.csv', header = None, names = ['Filename'] + self.videoClasses, skiprows = [0], index_col = 0)
+		dt = pd.read_csv(self.projFileManager.localMasterDir + '/prediction/ConfidenceMatrix.csv', header = None, names = ['Filename'] + self.videoClasses, skiprows = [0], index_col = 0)
 		softmax = dt.apply(scipy.special.softmax, axis = 1)
 		prediction = pd.concat([softmax.idxmax(axis=1).rename(self.mlFileManager.vModelID + '_pred'), softmax.max(axis=1).rename(self.mlFileManager.vModelID + '_conf')], axis=1)
 		prediction['ClipName'] = prediction.apply(lambda row: row.name.split('/')[-1], axis = 1)
-		allClusterData = pd.read_csv(self.prFileManager.localAllLabeledClustersFile, sep = ',')
+		allClusterData = pd.read_csv(self.projFileManager.localAllLabeledClustersFile, sep = ',')
 		allClusterData = pd.merge(allClusterData, prediction, how = 'left', left_on = 'ClipName', right_on = 'ClipName')
-		allClusterData.to_csv(self.prFileManager.localAllLabeledClustersFile, sep = ',')
+		allClusterData.to_csv(self.projFileManager.localAllLabeledClustersFile, sep = ',')
 
 		print('ML cluster classification complete,,Time: ' + str(datetime.datetime.now())) 
 
