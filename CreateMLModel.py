fb2de9847d1fcd3c6400c7bbe42e6e54dbca0b4a
Patrick McGrath <ptmcgrat@gmail.com>
2019-12-10 14:28:38 -0500

    Test

--- CreateMLModel.py
+++ CreateMLModel.py
@@ -3,8 +3,8 @@ import subprocess, pdb
 
 anFM_obj = FM().retAnFileManager()
 
-#labeledClipsDir = anFM_obj.prepareVideoAnnotation('10classLabels')
-labeledClipsDir = '/Users/pmcgrath7/Temp/CichlidAnalyzer/__AnnotatedData/LabeledVideos/10classLabels/LabeledClips/'
+labeledClipsDir = anFM_obj.prepareVideoAnnotation('10classLabels')
+#labeledClipsDir = '/Users/pmcgrath7/Temp/CichlidAnalyzer/__AnnotatedData/LabeledVideos/10classLabels/LabeledClips/'
 #pdb.set_trace()
 
-subprocess.run(['python3', 'Modules/MachineLearning/3D_resnet.py', '--data', labeledClipsDir])
+#subprocess.run(['python3', 'Modules/MachineLearning/3D_resnet.py', '--data', labeledClipsDir])
