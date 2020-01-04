15a1f6270702a4bdbbbf28963223c522169b6008
Lijiang Long <llong35@gatech.edu>
2020-01-03 21:37:09 -0500

    debug

--- Modules/MachineLearning/VideoLoader.py
+++ Modules/MachineLearning/VideoLoader.py
@@ -82,7 +82,6 @@ class VideoLoader(data.Dataset):
 
 
 trainset = VideoLoader('/data/home/llong35/Temp/CichlidAnalyzer/__AnnotatedData/LabeledVideos/10classLabels/LabeledClips/training', 'train', (90,112,112))
-pdb.set_trace()
 for i in range(trainset.__len__()):
 	if i == 6125:
 		pdb.set_trace()
